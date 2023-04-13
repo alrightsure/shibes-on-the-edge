4a1b53f955d5e60e3979f6eff5be222f79fec37f
Cullen Abbs <cullen0880@gmail.com>
2023-04-13 01:20:05 -0400

    fixed typo on 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,10 +3,10 @@ import { A } from "solid-start";
 export default function NotFound() {
     return (
         <main class="text-center mx-auto text-white  p-4">
-            <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Not Found</h1>
+            <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">404 Not Found</h1>
             <p class="leading-7 [&:not(:first-child)]:mt-6">No shibes here...</p>
             <span>
-                <A href="/" class="border-b-2">Take me to the sibes</A>
+                <A href="/" class="border-b-2">Take me to the shibes</A>
             </span>
         </main>
     );
